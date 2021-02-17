328e6650c29e3bbb52a72ceb46358f7ac80143da
Ishjot Singh <ishjotsinghahluwalia@gmail.com>
2021-02-18 00:19:41 +0530

    sort

--- src/App.js
+++ src/App.js
@@ -18,19 +18,44 @@ function App() {
     }
   ])
   const updateHandler = () => {
+
+    function sortByProperty(property) {
+      return function (a, b) {
+        if (a[property] < b[property])
+          return 1;
+        else if (a[property] > b[property])
+          return -1;
+
+        return 0;
+      }
+    }
     const id = 2;
     const reqTodo = todos.filter((todo) => (todo.id == id))[0]
     // before update
-    console.log(reqTodo)
-    reqTodo.reminder = false
+    // console.log(reqTodo)
+    // reqTodo.reminder = false
     // after update
-    console.log(reqTodo)
+    // console.log(reqTodo)
     // add
     //  setTodos([...todos, reqTodo])
     // delete 
     // setTodos(todos.filter((todo) => (todo.id !== id)))
     // update
     // setTodos(todos.filter((todo) => (todo.id === id ? todo : { ...todo, reqTodo })))
+    // sort desc
+    // const x = todos.sort(sortByProperty("id"))
+    // console.log(x)
+    // console.log(todos)
+    // const x = todos.sort(sortByProperty("id"))
+    // console.log(x)
+    // console.log(typeof (x))
+    // console.log(todos)
+    // console.log(typeof (todos))
+    const z = [...todos]
+    z.sort(sortByProperty("id"))
+    console.log(todos)
+    console.log(z)
+    setTodos(z)
   }
   return (
     <>
