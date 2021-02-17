d58605e6dbe175783717242019a91976cc9c4e60
Ishjot Singh <ishjotsinghahluwalia@gmail.com>
2021-02-17 23:52:01 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      "text": "Google Meet",
+      "day": "Monday",
+      "reminder": true,
+      "id": 1
+    },
+    {
+      "text": "College Viva",
+      "day": "Friday",
+      "reminder": true,
+      "id": 2
+    }
+  ])
+  const updateHandler = () => {
+    const id = 2;
+    const reqTodo = todos.filter((todo) => (todo.id == id))[0]
+    // before update
+    console.log(reqTodo)
+    reqTodo.reminder = false
+    // after update
+    console.log(reqTodo)
+    // add
+    //  setTodos([...todos, reqTodo])
+    // delete 
+    // setTodos(todos.filter((todo) => (todo.id !== id)))
+    // update
+    // setTodos(todos.filter((todo) => (todo.id === id ? todo : { ...todo, reqTodo })))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input type='button' onClick={updateHandler} value='Update' />
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo.id} {todo.text} {todo.day} {todo.reminder.toString()} </li>
+        ))
+        }
+      </ul>
+    </>
   );
 }
 
